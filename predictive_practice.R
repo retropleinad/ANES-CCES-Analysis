5f6615c3cef025e99f0d2af2c4756c8793de3997
Daniel Porter <73040840+retropleinad@users.noreply.github.com>
2023-04-24 13:48:31 -0400

    Add files via upload

--- predictive_practice.R
+++ predictive_practice.R
@@ -6,6 +6,7 @@ library(glmnet)
 library(gbm)
 library(randomForest)
 library(caTools)
+library(ecospat)
 
 
 # Bring in source data
@@ -24,10 +25,33 @@ ces['pid7_normalized'] = ces[, 'pid7'] / max(ces[, 'pid7'])
 head(ces[, 'pid7_normalized'])
 
 
-# Build gradient boosting model with k-fold control and regularization
-# Initial k-fold model
+# Build cross-validated gradient and ridge models
+
+# K-fold by itself
 kfold_control <- trainControl(method='cv', number=5)
 
 kfold_model <- train(pid7_normalized ~ educ + birthyr,
                      data=ces, method='lm', trControl=kfold_control)
 kfold_model
+
+# Gradient with k-fold
+# https://search.r-project.org/CRAN/refmans/ecospat/html/ecospat.cv.gbm.html
+gradient_model <- gbm(pid7_normalized ~ educ + birthyr,
+                      data = ces,
+                      distribution = 'bernoulli',
+                      cv.folds = 10,
+                      n.cores = 2)
+
+gradient_predict <- ecospat.cv.gbm(gbm.obj=gradient_model,
+                                   ces,
+                                   k=10,
+                                   cv.lim=10,
+                                   jack.knife=FALSE)
+
+# Cross-validated ridge regression
+# https://www.rdocumentation.org/packages/glmnet/versions/4.1-7/topics/cv.glmnet
+ridge_model <- cv.glmnet(pid7_normalized ~ educ + birthyr,
+                         data = ces,
+                         alpha=0)
+
+# Cross-validated
